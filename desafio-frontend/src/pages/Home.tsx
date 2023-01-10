2228b3a2ab7e8b2255404d692df3a6f282e7a405
aaguero96 <andre.luiz_1996@hotmail.com>
2023-01-10 20:40:22 -0300

    feat: create Home Page

---
+++ desafio-frontend/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import User from '../components/User';
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <User />
+    </div>
+  );
+}
+
+export default Home;
