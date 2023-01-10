51d32ac83c44026fcd94e395af5811f6820241b4
aaguero96 <andre.luiz_1996@hotmail.com>
2023-01-10 20:24:06 -0300

    feat: add button to go /login

--- desafio-frontend/src/pages/Register.tsx
+++ desafio-frontend/src/pages/Register.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Register from '../components/Register';
 
 function RegisterPage() {
+  const navigate = useNavigate();
+
+
   return (
     <div>
+      <button
+        onClick={() => {navigate("/login")}}
+      >
+        Voltar
+      </button>
       <Register />
     </div>
   );
