5dc58740d5a1cb3b3371246da790b400f4b36721
aaguero96 <andre.luiz_1996@hotmail.com>
2023-01-10 13:33:59 -0300

    feat: creat cryptPassword function

---
+++ desafio-frontend/src/utilities/crypt.ts
@@ -0,0 +1,17 @@
+import bcrypt from "bcryptjs";
+
+const cryptPassword = (password: string): string => {
+  const SALT_ROUNDS = 10;
+  const salt = bcrypt.genSaltSync(SALT_ROUNDS);
+  const hash = bcrypt.hashSync(password, salt);
+  return hash;
+}
+
+const comparePassword = (hash: string, password: string): boolean => {
+  return bcrypt.compareSync(password, hash);
+}
+
+export {
+  cryptPassword,
+  comparePassword,
+}
