ae5c7c2afc86f90d36fdba067b0e98496b4a0af2
aaguero96 <andre.luiz_1996@hotmail.com>
2023-01-10 20:39:34 -0300

    feat: create Header

---
+++ desafio-frontend/src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      <button>
+        Home
+      </button>
+      <button>
+        Usuários
+      </button>
+      <button>
+        Editar cadastro
+      </button>
+    </div>
+  );
+}
+
+export default Header;
