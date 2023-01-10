a1255a891280bef1d8bbe8ef087c40cf680c0ce3
aaguero96 <andre.luiz_1996@hotmail.com>
2023-01-09 22:40:13 -0300

    feat: create /users route

--- desafio-frontend/src/index.tsx
+++ desafio-frontend/src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Users from './pages/Users';
+import App from './App';
 
 const router = createBrowserRouter([
   {
+    path: "/users",
+    element: <Users />
+  },
+  {
     path: "/",
     element: <App />
-  },
+  }
 ])
 
 const root = ReactDOM.createRoot(
