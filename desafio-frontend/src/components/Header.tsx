9b241467b5f08501892572883c9245c2bb9f7edb
aaguero96 <andre.luiz_1996@hotmail.com>
2023-01-10 21:57:54 -0300

    feat: add navigate functionality

--- desafio-frontend/src/components/Header.tsx
+++ desafio-frontend/src/components/Header.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { UserState } from '../utilities/userInterface';
 
 function Header() {
+  const loggedUser = useSelector<UserState, UserState["user"]>((state) => state.user);
+
+  const navigate = useNavigate();
+
   return (
     <div>
-      <button>
+      <button
+        onClick={() => { navigate(`/home/${loggedUser?.id}}`) }}
+      >
         Home
       </button>
-      <button>
+      <button
+      >
         Usuários
       </button>
-      <button>
+      <button
+        onClick={() => { navigate(`/edit/${loggedUser?.id}}`) }}
+      >
         Editar cadastro
       </button>
     </div>
