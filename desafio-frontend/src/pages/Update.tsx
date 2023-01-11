e8573b5e9306e5351d0cfd3051f63f967c84e9c5
aaguero96 <andre.luiz_1996@hotmail.com>
2023-01-10 22:28:53 -0300

    feat: create update page

---
+++ desafio-frontend/src/pages/Update.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Update from '../components/Update';
+import Header from '../components/Header';
+
+function UpdatePage() {
+  return (
+    <div>
+      <Header />
+      <Update />
+    </div>
+  );
+}
+
+export default UpdatePage;
