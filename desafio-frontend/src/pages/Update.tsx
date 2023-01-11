99763b3902b3865921e298f1ba42302f1f862a73
aaguero96 <andre.luiz_1996@hotmail.com>
2023-01-10 23:14:12 -0300

    feat: add navigate to /login

--- desafio-frontend/src/pages/Update.tsx
+++ desafio-frontend/src/pages/Update.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Update from '../components/Update';
 import Header from '../components/Header';
+import { useSelector } from 'react-redux';
+import { UserState } from '../utilities/userInterface';
+import { useNavigate } from 'react-router-dom';
 
 function UpdatePage() {
+  const loggedUser = useSelector<UserState, UserState["user"]>((state) => state.user);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!loggedUser) {
+      navigate("/login");
+    }
+  })
+
   return (
     <div>
       <Header />
