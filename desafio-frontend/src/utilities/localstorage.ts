a36ec2b6e6c631b2821fd07903b4eb959f34556e
aaguero96 <andre.luiz_1996@hotmail.com>
2023-01-10 21:57:19 -0300

    feat: create localstorage functions

---
+++ desafio-frontend/src/utilities/localstorage.ts
@@ -0,0 +1,20 @@
+import { IUser } from "./userInterface";
+
+const localstore = window.localStorage;
+
+const setUserLocal = (user: IUser): void => {
+  localstore.setItem("user", JSON.stringify(user));
+};
+
+const getUserLocal = (): IUser | undefined => {
+  const users = localstore.getItem("user");
+  if (users !== null) {
+    return JSON.parse(users);
+  }
+  return undefined;
+}
+
+export {
+  setUserLocal,
+  getUserLocal,
+}
