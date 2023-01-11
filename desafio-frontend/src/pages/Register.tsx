89f52740953ec5175934515b538345cee6ab5a71
aaguero96 <andre.luiz_1996@hotmail.com>
2023-01-10 23:46:12 -0300

    chore: replace external api getter

--- desafio-frontend/src/pages/Register.tsx
+++ desafio-frontend/src/pages/Register.tsx
@@ -1,19 +1,8 @@
-import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import Register from '../components/Register';
-import { UserState } from '../utilities/userInterface';
 
 function RegisterPage() {
-  const loggedUser = useSelector<UserState, UserState["user"]>((state) => state.user);
-
   const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!loggedUser) {
-      navigate("/login");
-    }
-  })
 
   return (
     <div>
